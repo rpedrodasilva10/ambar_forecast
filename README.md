7161c839342ec757c674dea20a4dd728959ddd4b
Renan Pedro <rpedrodasilva10@gmail.com>
2019-06-08 19:07:36 -0300

    Criação dos endpoints e implementação dos métodos de leitura/gravação da API

--- README.md
+++ README.md
@@ -1,47 +1,49 @@
-# Teste técnico Ambar TECH
+# Teste t&eacute;cnico Ambar Tech
 
 ## Objetivo
 
-Implementar um webservice que utilize um serviço de previsão do tempo (http://apiadvisor.climatempo.com.br/doc/index.html#api-Forec
+Implementar um webservice que utilize um servi&ccedil;o de previs&atilde;o do tempo (http://apiadvisor.climatempo.com.br/doc/index.html#api-Forec
 ast-Forecast15DaysByCity), e persista os dados no banco de dados relacional (SQLite). 
 O backend deve fornecer ainda uma interface
 para consumo externo (API RESTful)
 
 # Design
-    Aplicação -> API REST -> WebService -> API Previsão do Tempo (Climatempo) -> Database
+    
+Aplica&ccedil;&atilde;o -> API REST -> WebService -> API Previs&atilde;o do Tempo (Climatempo) -> Database
 
 ![Design](/static/img/Design.PNG)
 
-# Regras de negócio
+# Regras de neg&oacute;cio
 
-1) O webservice receberá, através de uma interface REST, uma requisição da aplicação com o id de uma cidade como parâmetro;
+1) O webservice receber&aacute;, atrav&eacute;s de uma interface REST, uma requisi&ccedil;&atilde;o da aplica&ccedil;&atilde;o com o id de uma cidade como par&acirc;metro;
 ## Exemplo
     localhost:5000/cidade?id=<ID_DA_CIDADE>
 
 
-2) O webservice faz uma requisição à API do serviço de previsão do tempo, enviando os parâmetros id e token;
+2) O webservice faz uma requisi&ccedil;&atilde;o &agrave; API do servi&ccedil;o de previs&atilde;o do tempo, enviando os par&acirc;metros id e token;
 
 ## Exemplo
 
     http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/<ID_DA_CIDADE>/days/15?token=<TOKEN>
 
-    O token a ser utilizado é o   <u>b22460a8b91ac5f1d48f5b7029891b53</u>
+O token a ser utilizado &eacute; o   b22460a8b91ac5f1d48f5b7029891b53
 
 
 
-3) Persistir no banco de dados os campos (obtidos na resposta da requisição):
+3) Persistir no banco de dados os campos (obtidos na resposta da requisi&ccedil;&atilde;o):
 
 * Nome da cidade
 * Estado 
-* País 
+* Pa&iacute;s 
 * Data 
-* dados de chuva (probabilidade e precipitação) 
+* dados de chuva (probabilidade e precipita&ccedil;&atilde;o) 
 * campos min e max de temperatura.
-* A modelagem do banco é de livre escolha do usuário.
-4) Implementar uma nova ação que recebe os parâmetros *data_inicial* e *data_final* e, consultando no banco de dados, calcula e retorna
-os seguintes valores dentro do período especificado:
+* A modelagem do banco &eacute; de livre escolha do usu&aacute;rio.
+
+4) Implementar uma nova a&ccedil;&atilde;o que recebe os par&acirc;metros *data_inicial* e *data_final* e, consultando no banco de dados, calcula e retorna
+os seguintes valores dentro do per&iacute;odo especificado:
 ## Exemplo
     localhost:5000/analise?data_inicial=<DATA_INICIAL>&data_final=<DATA_FINAL>
 
-* A cidade que possui a maior temperatura máxima.
-* Média de precipitação por cidade.
+* A cidade que possui a maior temperatura m&aacute;xima.
+* M&eacute;dia de precipita&ccedil;&atilde;o por cidade.
--- forecast.py
+++ forecast.py
@@ -1,73 +1,172 @@
-from flask import Flask, g
-import sqlite3
+from flask import Flask, g, jsonify, request
+
+from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
 
+import requests
+import datetime
+import json
+import markdown
+import os
 
-# Instancia um app
+TOKEN = 'b22460a8b91ac5f1d48f5b7029891b53'
+
+# Instancia Flask, API, Database e Marshmallow
 app = Flask(__name__)
+api = Api(app)
+
+# Configurações
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fcast.db'
-db = SQLAlchemy(app)
+db = SQLAlchemy(app)  # DB
+ma = Marshmallow(app)  # Marshmallow
 
-"""
-TODO - Criar classe base
-Base = db.declarative_base()
-class Base(db.Model):
-    created = db.Column(db.DateTime)
-    updated = db.Column(db.DateTime)
-    nome =  db.Column(db.String(80), unique=True, nullable=False)
-"""
-#Models
-class Pais(db.Model):
-    __tablename__ = "pais"
-    id = db.Column(db.Integer, primary_key=True)
-    created = db.Column(db.DateTime)
-    updated = db.Column(db.DateTime)
-    nome =  db.Column(db.String(80), unique=True, nullable=False)
 
-    
-class Estado(db.Model):
-    """
-    Estados
-    """
-    __tablename__ = "estado"
-    id = db.Column(db.Integer, primary_key=True)
-    pais_id = db.Column(db.Integer, db.ForeignKey('pais.id'), nullable=False)
-    
-    uf = db.Column(db.String(2),  nullable=False, unique=True)
-    # TODO - implementar foreign key
-    capital = db.Column(db.String(80), unique=True, nullable=False)
-    created = db.Column(db.DateTime)
-    updated = db.Column(db.DateTime)
-    nome =  db.Column(db.String(80), unique=True, nullable=False)
-
-    
-
-class Cidade(db.Model):
-    """
-    Cidades
-    """
-    __tablename__ = "cidade"
-    id = db.Column(db.Integer, primary_key=True)
-    estado_id = db.Column(db.Integer, db.ForeignKey('estado.id'), nullable=False)
-    
-    created = db.Column(db.DateTime)
-    updated = db.Column(db.DateTime)
-    nome =  db.Column(db.String(80), unique=True, nullable=False)
-
-   
-
+# Models/Classes
 class Resposta(db.Model):
     __tablename__ = "resposta"
     id = db.Column(db.Integer, primary_key=True)
-    cidade_id = db.Column(db.Integer, db.ForeignKey('cidade.id'), nullable=False)
-    pais_id = db.Column(db.Integer, db.ForeignKey('pais.id'), nullable=False)
-    data = db.Column(db.DateTime, nullable=False)
-    prob_chuva = db.Column(db.Float, nullable=False)
-    preci_chuva = db.Column(db.Float, nullable=False)
-    temp_min = db.Column(db.Float, nullable=False)
-    temp_max = db.Column(db.Float, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    state = db.Column(db.String(80), nullable=False)
+    city = db.Column(db.String(80), nullable=False)
+    country = db.Column(db.String(80), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    rain_prob = db.Column(db.Float, nullable=False)
+    rain_prec = db.Column(db.Float, nullable=False)
+    min_temp = db.Column(db.Float, nullable=False)
+    max_temp = db.Column(db.Float, nullable=False)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
+class RespostaSchema(ma.ModelSchema):
+    class Meta:
+        model = Resposta
+
+
+resposta_schema = RespostaSchema()
+respostas_schema = RespostaSchema(many=True)
+
+
+class Forecast(Resource):
+    """Forecast lê o banco de dados"""
+
+    # Códigos de ERRO e mensagens
+    erros = {
+        1: "Data inicial deve ser maior ou igual que a data final.",
+        2: "Datas devem ser passadas no formato AAAA-MM-DD."
+    }
+
+    def date_is_valid(self, date, mask='%Y-%m-%d'):
+        rv = True
+        # Inclusão do parâmetro mask para validar dinâmicamente ao formato desejado
+        try:
+            datetime.datetime.strptime(date, mask)
+        except ValueError:
+            rv = False
+        return rv
+
+    def validate(self, data_inicial, data_final):
+        """Checa se as datas recebidas estão dentro do formato (AAAA-MM-DD) e range (inicial <= final) válidos."""
+        """ TODO - Validar datas REAIS """
+        err_n = 0
+        if self.date_is_valid(data_inicial) and self.date_is_valid(data_final):
+            if data_inicial > data_final:
+                err_n = 1
+                return False, err_n
+            else:
+                return True, err_n
+        else:
+            err_n = 2
+            return False, err_n
+
+    def get_max_temp(self, filtro_ini, filtro_fim):
+        """Retorna maior temperatura máxima dentro do período informado."""
+        respostas = db.session.query(
+            Resposta.state, Resposta.country, Resposta.city, Resposta.name, db.func.max(
+                Resposta.max_temp).label('max_temp')
+        ).filter(
+            Resposta.date.between(filtro_ini, filtro_fim)).group_by(Resposta.city, Resposta.name, Resposta.state, Resposta.country).first()
+
+        rv = resposta_schema.dump(respostas)
+        return rv
+
+    def get_avg_precipitation(self, filtro_ini, filtro_fim):
+        """Retorna precipitação e média dados da cidade, dentro do período informado"""
+        respostas = db.session.query(
+            Resposta.state, Resposta.country, Resposta.city, Resposta.name, db.func.avg(
+                Resposta.rain_prec).label('rain_prec')
+        ).filter(
+            Resposta.date.between(filtro_ini, filtro_fim)).group_by(
+                Resposta.city, Resposta.name, Resposta.state, Resposta.country).order_by(Resposta.name)
+
+        rv = respostas_schema.dump(respostas)
+        return rv
+
+    def get(self):
+        data_inicial = request.args.get('data_inicial')
+        data_final = request.args.get('data_final')
+
+        continua, err_n = self.validate(data_inicial, data_final)
+        if continua and err_n == 0:
+            filtro_ini = datetime.datetime.strptime(
+                data_inicial, '%Y-%m-%d')
+            filtro_fim = datetime.datetime.strptime(
+                data_final, '%Y-%m-%d')
+        else:
+            return {'mensagem': self.erros[err_n], 'data': [data_inicial, data_final]}, 400
+
+        # Cidade com maior temperatura máxima.
+        max_temp_data = self.get_max_temp(filtro_ini, filtro_fim)
+        # Média de precipitação por cidade.
+        avg_preci_data = self.get_avg_precipitation(filtro_ini, filtro_fim)
+
+        return {'mensagem': 'Sucesso', 'data': {'max_temp_data': max_temp_data.data, 'avg_preci_data': avg_preci_data.data}}, 200
+
+    def save(self):
+        pass
+
+
+"""TODO Criar método que busca cidade (consome api) e grava dados no banco"""
+@app.route('/cidade')
+def get_forecast():
+    """Recebe uma cidade (ID) consome API e persiste informações sobre o clima no banco de dados"""
+    params = {'token': TOKEN}
+    city_id = str(request.args.get('id', None))
+    res = requests.get('http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/' +
+                       city_id + '/days/15', params=params)
+    if res.status_code == 200:
+        js = res.json()
+        name = js['name']
+        state = js['state']
+        city = js['id']
+        country = js['country']
+        data = js['data']  # É uma lista
+
+        for element in data:
+            date_time_obj = datetime.datetime.strptime(
+                element['date'], '%Y-%m-%d')
+            rain_prob = element['rain']['probability']
+            rain_prec = element['rain']['precipitation']
+            max_temp = element['temperature']['max']
+            min_temp = element['temperature']['min']
+            resposta = Resposta(name=name, state=state, city=city, country=country, date=date_time_obj.date(),
+                                rain_prec=rain_prec, rain_prob=rain_prob, max_temp=max_temp, min_temp=min_temp)
+            db.session.add(resposta)
+
+        db.session.commit()
+        return jsonify({'message': 'success'}), res.status_code
+    else:
+        return jsonify({'message': 'not found'}), res.status_code
+
+
+@app.route("/", methods=['GET'])
+def index():
+    with open("./README.md", 'r') as markdown_file:
+        content = markdown_file.read()
+
+    return markdown.markdown(content)
+
+
+api.add_resource(Forecast, '/analise/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
